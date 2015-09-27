50a92e96b79de408051d839858372fa3c8d13e51
fabian <famargon@inf.upv.es>
2015-09-27 17:27:23 +0200

    notas mejoradas, añadir, listar y buscar

--- misNotas.php
+++ misNotas.php
@@ -6,9 +6,17 @@
 <H1>Nueva nota</H1>
 
 <FORM ACTION="procesaNotas.php" METHOD="POST" accept-charset=utf-8>
-<INPUT TYPE="text" NAME="notaNueva"><BR>
+<INPUT TYPE="text" NAME="notaNueva">
 
-<INPUT TYPE="submit" VALUE="Enviar">
+<INPUT TYPE="submit" NAME="boton" VALUE="Enviar" ><BR>
+
+Listar notas 
+
+<INPUT TYPE="submit" NAME="boton" VALUE="Ver"><BR>
+
+Buscar nota por contenido:
+<INPUT TYPE="text" NAME="palabro">
+<INPUT TYPE="submit" NAME="boton" VALUE="Buscar"><BR>
 </FORM>
 </body>
 </html> 
--- procesaNotas.php
+++ procesaNotas.php
@@ -3,42 +3,101 @@
    <title>Mis notas</title>
 </head>
 <body>
-<H1>Lista de notas</H2>
 	<?php
-
-	$nota=$_POST['notaNueva'];
-
-
-	if(!$enlace = mysql_connect('localhost','root','0213'))
-	{
-	echo 'No pudo conectarse a mysql';
-	    exit;
-	}
-	if (!mysql_select_db('notasDB', $enlace)) {
-	    echo 'No pudo seleccionar la base de datos';
-	    exit;
-	}
-	$sqlID='select MAX(id) from notas';
-	$resultadoID=mysql_query($sqlID,$enlace);
-	if (!$resultadoID) {
-	    echo "Error de BD, no se pudo consultar la base de datos\n";
-	    echo "Error MySQL:   mysql_error()";
-	    exit;
-	}
-	$row=mysql_fetch_row($resultadoID);
-	$nuevoID=$row[0]+1;
-	$sqlInsert="insert into notas (id,valor) values ('$nuevoID','$nota')";
-	$resultadoInsert = mysql_query($sqlInsert,$enlace);
+	switch($_POST['boton']){
 	
-	$sqlSelect="select * from notas order by id desc";
-	$resultadoSelect=mysql_query($sqlSelect,$enlace);
+	case "Enviar":
+	?>
+		<H1>Lista de notas</H1>
+	<?php
+		if(!$enlace = mysql_connect('localhost','root','0213')){
+	            echo 'No pudo conectarse a mysql';
+        	    exit;
+        	}
+        	if (!mysql_select_db('notasDB', $enlace)) {
+        	    echo 'No pudo seleccionar la base de datos';
+        	    exit;
+        	}		
 	
-	while($fila=mysql_fetch_array($resultadoSelect,MYSQL_NUM)){
-		echo "$fila[0] , $fila[1]<br>";
-	}
+		$sqlID='select MAX(id) from notas';
+		$resultadoID=mysql_query($sqlID,$enlace);
+		if (!$resultadoID) {
+		    echo "Error de BD, no se pudo consultar la base de datos\n";
+		    echo "Error MySQL:   mysql_error()";
+		    exit;
+		}
+			
+		$row=mysql_fetch_row($resultadoID);
+		$nuevoID=$row[0]+1;
 
- 	mysql_free_result($resultadoSelect);
-	mysql_free_result($resultadoID);
+		$nota=$_POST['notaNueva'];	
+	
+		$sqlInsert="insert into notas (id,valor) values ('$nuevoID','$nota')";
+		$resultadoInsert = mysql_query($sqlInsert,$enlace);
+	
+		$sqlSelect="select * from notas order by id desc";
+		$resultadoSelect=mysql_query($sqlSelect,$enlace);
+		
+		while($fila=mysql_fetch_array($resultadoSelect,MYSQL_NUM)){
+			echo "$fila[0] , $fila[1]<br>";
+		}
+
+ 		mysql_free_result($resultadoSelect);
+		mysql_free_result($resultadoID);
+		
+		break;
+	
+	case "Ver":
+	?>
+		<H1>Lista de notas</H1>
+	<?php
+		if(!$enlace = mysql_connect('localhost','root','0213')){
+              	    echo 'No pudo conectarse a mysql';
+        	    exit;
+        	}
+        	if (!mysql_select_db('notasDB', $enlace)) {
+        	    echo 'No pudo seleccionar la base de datos';
+        	    exit;
+        	}
+
+		$sqlSelect="select * from notas order by id desc";
+        	$resultadoSelect=mysql_query($sqlSelect,$enlace);
+
+        	while($fila=mysql_fetch_array($resultadoSelect,MYSQL_NUM)){
+                	echo "$fila[0] , $fila[1]<br>";
+        	}	
+
+       		mysql_free_result($resultadoSelect);
+
+		break;
+	case "Buscar":
+	?>
+		<H1>Notas encontradas</H1>
+	<?php
+		 if(!$enlace = mysql_connect('localhost','root','0213')){
+                    echo 'No pudo conectarse a mysql';
+                    exit;
+                }
+                if (!mysql_select_db('notasDB', $enlace)) {
+                    echo 'No pudo seleccionar la base de datos';
+                    exit;
+                }
+		
+		$palabro=$_POST['palabro'];	
+
+                $sqlSelect="select * from notas where valor like '%$palabro%' order by id desc";
+                $resultadoSelect=mysql_query($sqlSelect,$enlace);
+
+                while($fila=mysql_fetch_array($resultadoSelect,MYSQL_NUM)){
+                        echo "$fila[0] , $fila[1]<br>";
+                }
+
+                mysql_free_result($resultadoSelect);
+
+                break;
+
+	
+	}
 	?>
 
 </body>
